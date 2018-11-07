1ddb2747256ac71736b47c23880ab9b0e9713fa4
Irfan Smajevic <smajevic.irfan@gmail.com>
2018-11-08 00:37:32 +0100

    Created class Studij

---
+++ src/ba/unsa/etf/rpr/tutoriaj04/Studij.java
@@ -0,0 +1,12 @@
+package ba.unsa.etf.rpr.tutoriaj04;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Studij {
+    private List<Semestar> semestri;
+
+    public Studij() {
+        this.semestri = new ArrayList<>();
+    }
+}
