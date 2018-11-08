13568fd36b4c39ff09661736b1c24bef073eb53c
Irfan Smajevic <smajevic.irfan@gmail.com>
2018-11-08 10:21:19 +0100

    Removed class Semestar

--- src/ba/unsa/etf/rpr/tutoriaj04/Studij.java
+++ src/ba/unsa/etf/rpr/tutoriaj04/Studij.java
@@ -4,9 +4,5 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Studij {
-    private List<Semestar> semestri;
-
-    public Studij() {
-        this.semestri = new ArrayList<>();
-    }
+    
 }
